e7f066e1c4bc93c8786ea48138754a2e8024a700
VANESSA ROCHA <vanessa.cardoso@rede.ulbra.br>
2023-11-28 10:29:41 -0300

    .

---
+++ src/Components/Header.js
@@ -0,0 +1,8 @@
+function Header({ title }) {
+  return (
+    <div style={{ width: "100%", height: "60px", backgroundColor: "black" }}>
+      <h1>{title}</h1>
+    </div>
+  );
+}
+export default Header;
