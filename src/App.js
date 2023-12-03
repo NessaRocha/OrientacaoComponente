9c30ec3f352d4b1fb9f481f65446f1d9df3a4bbd
VANESSA ROCHA <vanessa.cardoso@rede.ulbra.br>
2023-12-03 20:32:39 -0300

    atualizacao

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
-import Cadastrar from "./Pages/Cadastrar";
-import DetalheDoProduto from "./Pages/DetalheDoProduto";
-import Home from "./Pages/Home";
-import Produtos from "./Pages/Produtos";
+import Cadastrar from "./pages/Cadastrar";
+import DetalheDoProduto from "./pages/DetalheDoProduto";
+import Home from "./pages/Home";
+import Produtos from "./pages/Produtos";
+import { ProductContext } from "./contexts/productContext";
 
 function App() {
+  const [product, setProduct] = useState();
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/produtos" element={<Produtos />} />
-        <Route path="/detalhesProdutos" element={<DetalheDoProduto />} />
-        <Route path="/cadastrar" element={<Cadastrar />} />
-      </Routes>
-    </BrowserRouter>
+    <ProductContext.Provider value={{ product, setProduct }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/produtos" element={<Produtos />} />
+          <Route path="/detalhesProdutos" element={<DetalheDoProduto />} />
+          <Route path="/cadastrar" element={<Cadastrar />} />
+        </Routes>
+      </BrowserRouter>
+    </ProductContext.Provider>
   );
 }
 
